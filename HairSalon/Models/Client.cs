1cbcfd068bbc9e89723da712766e8ec9af398d98
Tanvi Garg <tanvi.garg23@gmail.com>
2018-12-07 12:15:51 -0800

    get all client list empty test pass

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -42,8 +42,27 @@ namespace HairSalon.Models
 
     public static List<Client> GetAll()
     {
-      Client dummyClient = new Client("dummy client", "503-XXX-XXXX", 1);
-      List<Client> allClients = new List<Client> { dummyClient };
+      List<Client> allClients = new List<Client> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM clients;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int clientId = rdr.GetInt32(0);
+        string clientName = rdr.GetString(1);
+        string clientPhone = rdr.GetString(2);
+        int stylistId = rdr.GetInt32(3);
+        Client newClient = new Client(clientName, clientPhone, stylistId, clientId);
+        allClients.Add(newClient);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+
       return allClients;
     }
 
