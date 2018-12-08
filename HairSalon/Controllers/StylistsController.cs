e33d4638abac34f47d7ea846bbf578e5a72d44e5
Tanvi Garg <tanvi.garg23@gmail.com>
2018-12-08 01:01:59 -0800

    stylist controller edit route correct view test pass

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -44,8 +44,7 @@ namespace HairSalon.Controllers
     public ActionResult Edit(int id)
     {
       Stylist stylist = Stylist.Find(id);
-      //return View(stylist);
-      return new EmptyResult();
+      return View(stylist);
     }
 
 
