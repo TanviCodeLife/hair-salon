48401c90dd02fc0a296ec640f891807376c36a8b
Tanvi Garg <tanvi.garg23@gmail.com>
2018-12-07 23:03:08 -0800

    fail home controller index route correct view test

--- HairSalon.Tests/ControllerTests/HomeControllerTest.cs
+++ HairSalon.Tests/ControllerTests/HomeControllerTest.cs
@@ -7,9 +7,20 @@ using HairSalon.Models;
 
 namespace HairSalon.Tests
 {
-    [TestClass]
-    public class HomeControllerTest
+  [TestClass]
+  public class HomeControllerTest
+  {
+    [TestMethod]
+    public void Index_ReturnsCorrectView_True()
     {
-
+      //Arrange
+      HomeController controller = new HomeController();
+      //Act
+      var indexView = controller.Index();
+      //Assert
+      Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+      Console.WriteLine(typeof(ViewResult));
     }
+
+  }
 }
