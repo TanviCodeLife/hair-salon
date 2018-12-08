c0e29168fb6cd5ad78a1d3087612b8f14072e178
Tanvi Garg <tanvi.garg23@gmail.com>
2018-12-07 23:03:51 -0800

    pass home controller index route correct view test

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace HairSalon.Controllers
         [HttpGet("/")]
         public ActionResult Index()
         {
-            return new EmptyResult();
+            return View();
         }
     }
 }
