4408eb95053693f80d6c843ff8fb2272e299fceb
Tanvi Garg <tanvi.garg23@gmail.com>
2018-12-08 23:24:49 -0800

    stylist controller update route correct view type test - pass

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -52,8 +52,8 @@ namespace HairSalon.Controllers
     {
       Stylist newStylist = Stylist.Find(id);
       newStylist.Edit(name);
-      //return RedirectToAction("Index");
-      return new EmptyResult();
+      return RedirectToAction("Index");
+      //return View(0);
     }
 
 
